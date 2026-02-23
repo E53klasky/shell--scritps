6425a91cd5e0e30ba483e74b2b8087f041e0da42
E53klasky <klaskyethan@gmail.com>
2026-02-22 20:36:06 -0500

    adding just a subnect so now it is nice to use

--- email.sh
+++ email.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-FROM="eklasky@klaskyethan@gmail.com"
-TO="klaskyethan@gmail.com"
+FROM="klaskyethan@gmail.com"
+TO="hklasky@gmail.com"
 
-cat <<EOF | /usr/sbin/sendmail -i -t -v
+cat <<EOF | /usr/sbin/sendmail -i -t
 From: ${FROM}
 To: ${TO}
-Subject: Test Email
+Subject: Homework Update test
 Date: $(date -R)
 Message-ID: <$(date +%s).$$@klaskyethan.gmail.com>
 MIME-Version: 1.0
 Content-Type: text/plain; charset=UTF-8
 
-Hi Ethan,
+Hi Mom,
 
-This is a test email sent from the terminal using sendmail.
+I finially can send emails from the terminal.
 
 Best,
 Ethan
